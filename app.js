315b03b7d6cb0f5dfa8330792003d48dc2e2cb1e
Ubaidullah-M <111274816+Ubaidullah-M@users.noreply.github.com>
2023-07-11 01:34:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const reviews = [
+    {
+        id: 0,
+        name: 'Ayuba Dauda',
+        job: 'CTO',
+        img: "Ayuba.jpg"
+    },
+    
+    {
+        id: 1,
+        name: 'Muhammad Umar Isah',
+        job: 'Director',
+        img: "muhd.jpg"
+    },
+    
+    {
+        id: 2,
+        name: 'Hyedima',
+        job: 'Post. Senior Software Enngineer',
+        img: "hyedima.jpg"
+    },
+    
+    {
+        id: 3,
+        name: 'Hindu Bukar Biu',
+        job: 'Legal Team Lead',
+        img: "Hindu.jpg"
+    },
+    {
+        id: 4,
+        name: 'Barakat Usman',
+        job: 'Devop Engineerr',
+        img: "Barakat.jpg"
+    },
+    {
+        id: 5,
+        name: 'Josua Alex',
+        job: 'Senior Software Engineer',
+        img: "Joshua.jpg"
+    }
+]
